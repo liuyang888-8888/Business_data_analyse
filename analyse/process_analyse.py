30279e8a52d0362dbd3a71fe51c0c5661e8d5cb2
Sunnyln <691255995@qq.com>
2024-05-10 13:37:25 +0800

    init

--- analyse/process_analyse.py
+++ analyse/process_analyse.py
@@ -2,7 +2,7 @@
 #  道阻且长，行则将至
 # -----Sunnyln---
 #  5/10/2024  1:28 PM
-from LIb import config
+from Lib import config
 
 merged_data_path=config.merged_behavior_path
 demographic_csv_path=config.demographic_csv_path
--- analyse/timestamp_analyse.py
+++ analyse/timestamp_analyse.py
@@ -2,6 +2,6 @@
 #  道阻且长，行则将至
 # -----Sunnyln---
 #  5/10/2024  1:27 PM
-from LIb import config
+from Lib import config
 merged_data_path=config.merged_behavior_path
 demographic_csv_path=config.demographic_csv_path
